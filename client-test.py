38584742c81f5105c89d15533f760232a2b5de6a
Rizqi Okta Ekoputris <rizqi.okta@gmail.com>
2016-01-04 20:12:38 +0500

    ^_^

--- client-test.py
+++ client-test.py
@@ -1,11 +1,13 @@
 import socket
+import sys
 
 # create socket and connect to server
 server_address = ('localhost', 8080)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 # send string to server and close socket
-client_socket.send('Hi ... ')
+message = sys.stdin.readline()
+client_socket.send(message)
 resp=client_socket.recv(1024)
 print resp 
 client_socket.close()
--- client-test2.py
+++ client-test2.py
@@ -5,7 +5,8 @@ server_address = ('localhost', 8080)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 # send string to server and close socket
-client_socket.send('Hi ... ')
+message=sys.stdin.readline()
+client_socket.send(message)
 resp=client_socket.recv(1024)
 print resp 
 client_socket.close()
