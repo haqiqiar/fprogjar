117c6e22568a0f4d5d2163dc9126926fe0319ad7
Ayu Kardina <ayukardina13@mhs.if.its.ac.id>
2016-01-05 12:10:37 +0700

    Updated

--- GET/genheader.py
+++ GET/genheader.py
@@ -16,7 +16,7 @@ namefile='index.html'
 def _content_length(namefile):
     num_lines = 0
     num_words = 0
-    num_chars = 0
+    num_chars=0
     with open(namefile, 'r') as f:
         for line in f:
             words = line.split()
@@ -27,43 +27,39 @@ def _content_length(namefile):
 
             return num_chars
             
-<<<<<<< HEAD
-def _gen_headers(code, namefiles):
-=======
 def _gen_headers(code, namefile):
->>>>>>> 256137dfaef676eef5df1f9994669837a33b13fd
     h=''
     
     current_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
 
     if (code == 200):
         h = 'HTTP/1.1 200 OK\n'
-        h += 'Date: ' + current_date +'\n'
-        h += 'Server: Simple-Python-HTTP-Server\n'
-        h += 'Last-Modified: ' + time.ctime(os.path.getmtime(namefile)) +'\n'
-        h += 'Content-Length: ' + str(_content_length(namefile)) +'\n'
-        h += 'Connection: close'
+        h += 'Date: ' + current_date +'\r\n'
+        h += 'Server: Simple-Python-HTTP-Server\r\n'
+        h += 'Last-Modified: ' + time.ctime(os.path.getmtime(namefile)) +'\r\n'
+        h += 'Content-Length: ' + str(_content_length(namefile)) +'\r\n'
+        h += 'Connection: close\r\n'
     elif(code == 404):
-        h = 'HTTP/1.1 404 Not Found\n'
-        h += 'Date: ' + current_date +'\n'
-        h += 'Server: Simple-Python-HTTP-Server\n'
-        h += 'Content-Length: ' + str(_content_length(namefile)) +'\n'
-        h += 'Connection: close'
+        h = 'HTTP/1.1 404 Not Found\r\n'
+        h += 'Date: ' + current_date +'\r\n'
+        h += 'Server: Simple-Python-HTTP-Server\r\n'
+        h += 'Content-Length: ' + str(_content_length(namefile)) +'\r\n'
+        h += 'Connection: close\r\n'
     elif(code == 403):
-        h = 'HTTP/1.1 403 Forbidden\n'
-        h += 'Date: ' + current_date +'\n'
-        h += 'Server: Simple-Python-HTTP-Server\n'
-        h += 'Connection: Keep-Alive'
+        h = 'HTTP/1.1 403 Forbidden\r\n'
+        h += 'Date: ' + current_date +'\r\n'
+        h += 'Server: Simple-Python-HTTP-Server\r\n'
+        h += 'Connection: Keep-Alive\r\n'
     elif(code == 500):
-        h = 'HTTP/1.1 500 Internal Server Error\n'
-        h += 'Date: ' + current_date +'\n'
-        h += 'Server: Simple-Python-HTTP-Server\n'
-        h += 'Connection: close'
+        h = 'HTTP/1.1 500 Internal Server Error\r\n'
+        h += 'Date: ' + current_date +'\r\n'
+        h += 'Server: Simple-Python-HTTP-Server\r\n'
+        h += 'Connection: close\r\n'
     elif(code == 301):
-        h = 'HTTP/1.1 301 Moved Permanently\n'
-        h += 'Date: ' + current_date +'\n'
-        h += 'Server: Simple-Python-HTTP-Server\n'
-        h += 'Location: http://' + server_address[0] + ':' + str(server_address[1]) + namefile + '/'
+        h = 'HTTP/1.1 301 Moved Permanently\r\n'
+        h += 'Date: ' + current_date +'\r\n'
+        h += 'Server: Simple-Python-HTTP-Server\r\n'
+        h += 'Location: http://' + server_address[0] + ':' + str(server_address[1]) + namefile + '/' + '\r\n'
 
     print h
 
