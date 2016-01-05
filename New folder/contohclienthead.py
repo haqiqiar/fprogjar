cf190ff6917b95f031dabf77a165f0516e610142
Rizqi Okta Ekoputris <rizqi.okta@gmail.com>
2016-01-05 12:23:31 +0500

    new folder buat jaga jaga

---
+++ New folder/contohclienthead.py
@@ -0,0 +1,24 @@
+__author__ = 'zq'
+
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("localhost",8080))
+
+
+s.send("HEAD / HTTP/1.0\r\nHost: localhost:80\n\n")
+content = ""
+while True:
+        resp = s.recv(1024)
+        if resp == "": break
+        print resp
+        content += resp
+        #file.write(resp)
+
+s.close()
+print content
+#print "Content : " + content
+temporarySplit = content.split('\r\n\r\n')
+responseHeader = temporarySplit[0]
+
+#print responseHeader
